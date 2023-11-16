0ddf022da3d2d82979092293e05e6d8cc878a376
ycssona <ycssona@gmail.com>
2023-11-16 11:26:29 +0530

    Implementing Error & Success messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -26,7 +26,7 @@ const controlRecipes = async function () {
     // 2) Rendering the recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    recipeView.renderError();
   }
 };
 
--- src/js/model.js
+++ src/js/model.js
@@ -26,5 +26,6 @@ export const loadRecipe = async function (id) {
   } catch (err) {
     // Temp error handling
     console.log(`${err} 💥💥💥💥`);
+    throw err;
   }
 };
