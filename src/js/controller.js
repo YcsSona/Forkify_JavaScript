e5f63af997dee2a90836b7b4ea9f1a7e465c7679
ycssona <ycssona@gmail.com>
2023-11-16 10:32:09 +0530

    Event Handlers in MVC: Pub-Sub Pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -26,12 +26,13 @@ const controlRecipes = async function () {
     // 2) Rendering the recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function () {
+  // Subscriber
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
